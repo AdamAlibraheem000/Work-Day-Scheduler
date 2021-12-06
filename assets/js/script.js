041dd89e46ca7aef2d9407a5782f7e73c41b0c68
AdamAlibraheem <shrineknight000@gmail.com>
2021-12-06 18:54:42 -0500

    fixed local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,36 @@
 var rightNow = moment().format("MMM Do YYYY");
-// alert(rightNow);
-
 $("#currentDay").text(rightNow);
+var arr = [];
+var currentHour = moment().hour();
+
+function setTime() {
+  for (var i = 9; i < 18; i++) {
+    if (currentHour < i) {
+      $("#" + i).addClass("future");
+    } else if (currentHour === i) {
+      $("#" + i).addClass("present");
+    } else if (currentHour > i) {
+      $("#" + i).addClass("past");
+    }
+  }
+}
+
+setTime();
+
+$(".saveBtn").on("click", function () {
+  var id = $(this).attr("data-id");
+  var text = $("#" + id).val();
+  console.log(text);
+  localStorage.setItem(id, text);
+});
+
+function displayVals() {
+  for (let i = 0; i < 9; i++) {
+    if (localStorage.getItem(i)) {
+      let text = localStorage.getItem(i);
+      $("#" + i).val(text);
+    }
+  }
+}
+
+displayVals();
